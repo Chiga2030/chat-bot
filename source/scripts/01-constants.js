0b2e235b5e3e566d9f34b9aada9aceb977dac470
Антон Ткачук <chiga2030@gmail.com>
2020-10-21 22:33:48 +0300

    modify chat from down to up

--- source/scripts/01-constants.js
+++ source/scripts/01-constants.js
@@ -1,2 +1,3 @@
 const inputText = document.querySelector('.chat-window__text-buble-input');
 const submitBtn = document.querySelector('.chat-window__text-buble-button');
+const chatHistory = document.querySelector('.chat-window__history');
---
+++ source/scripts/04-create-user-message.js
@@ -0,0 +1,25 @@
+const counter = makeCounter();
+
+const createMessage = (textMessage) => {
+	const messageTemplate = `
+			<div class='message__avatar message__avatar_user'></div>
+			<div class='message__buble message__buble_user'>${textMessage}</div>
+	`;
+	const message = document.createElement('div');
+	message.classList.add('message');
+	message.style.order = counter();
+	message.innerHTML = messageTemplate;
+
+	return message;
+}
+
+const postingMessage = (text) => {
+	chatHistory.appendChild(createMessage(text))
+};
+
+postingMessage('test')
+postingMessage('test2')
+postingMessage('test3')
+postingMessage('test4')
+postingMessage('test5')
+postingMessage('test6')
