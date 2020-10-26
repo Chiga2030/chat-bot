3b822e04b6bf343e80e219eda7129b7bcddc42a3
Антон Ткачук <chiga2030@gmail.com>
2020-10-26 21:49:46 +0300

    remove comments

--- source/scripts/03-2-search-last-message.js
+++ source/scripts/03-2-search-last-message.js
@@ -3,6 +3,3 @@ const searchLastElement = (search) => {
 
 	return el[el.length-1];
 }
-
-// const lastMessage = searchLastElement('.message');
-// const lastBuble = searchLastElement('.message__buble_user');
