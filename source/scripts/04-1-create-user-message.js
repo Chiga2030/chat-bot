6d7111ed70240310c9d0180ce7b773598b426fff
Антон Ткачук <chiga2030@gmail.com>
2020-10-26 21:51:14 +0300

    remove comment code

--- source/scripts/04-1-create-user-message.js
+++ source/scripts/04-1-create-user-message.js
@@ -12,37 +12,3 @@ newMessage = () => {
 
 	return message;
 }
-
-
-
-
-
-
-
-
-
-
-// class Message {
-// 	constructor (from = 'user', textMessage = '', build = true) {
-// 		this.from = from;
-// 		this.textMessage = textMessage;
-// 		this.build = build;
-// 	}
-
-// 	messageTemplate = () => `
-// 			<div class='message__avatar message__avatar_${this.from}'></div>
-// 			<div class='message__buble message__buble_${this.from}'>${this.textMessage}</div>
-// 	`;
-
-// 	toHtml = () => {
-// 		const message = document.createElement('div');
-// 		message.classList.add('message');
-// 		message.style.order = orderMessage() - 1;
-// 		message.innerHTML = this.messageTemplate();
-
-// 		return message;
-// 	}
-// }
-
-// const userMessage = new Message('user', '');
-
