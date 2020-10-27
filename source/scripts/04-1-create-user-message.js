6675597beeb29f5229e7736784a4aac6d71ae4be
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-10-27 13:21:13 +0300

    modify default message

--- source/scripts/04-1-create-user-message.js
+++ source/scripts/04-1-create-user-message.js
@@ -1,4 +1,4 @@
-newMessage = (text) => {
+newMessage = (text = '<span class="dotted-anim">.</span><span class="dotted-anim">.</span><span class="dotted-anim">.</span><span class="dotted-anim">.</span>') => {
 	const userTemplate = `
 			<div class='message__avatar message__avatar_user'></div>
 			<div class='message__buble message__buble_user'>${text}</div>
