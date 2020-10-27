46a3ae5cf2304104095bdb69954bfc55a895e9db
Антон Ткачук <chiga2030@gmail.com>
2020-10-27 20:16:53 +0300

    added variable input form for form reset

--- source/scripts/01-constants.js
+++ source/scripts/01-constants.js
@@ -1,4 +1,5 @@
 let inputText = searchElement('.chat-window__text-buble-input');
+const inputForm = searchElement('.chat-window__text-buble');
 const submitBtn = searchElement('.chat-window__text-buble-button');
 const chatHistory = searchElement('.chat-window__history');
 const order = makeDecrementor();	// обратный счетчик для упорядочивания сообщений снизу вверх
