5fde9976527f27093a9eb7b58fb50ce1aff2b5bb
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-10-28 08:56:39 +0300

    testing configured express and axample fetch query

--- source/scripts/05-fetch-to-server.js
+++ source/scripts/05-fetch-to-server.js
@@ -1,15 +1,19 @@
-// const url = 'http://localhost:300/request-to-bot';
-const data = { userName: 'example', userAge: 'sample' };
-const url = 'http://localhost:300/request-to-bot';
+const data = { userName: 'example2', userAge: 'sample' };
+const url = 'http://localhost:3000/request-to-bot';
 
+(async () => {
 
 let response = await fetch(url, {
   method: 'POST',
+  // mode: 'cors',
   headers: {
     'Content-Type': 'application/json;charset=utf-8'
   },
   body: JSON.stringify(data)
 });
 
-let result = await response.json();
-alert(result.message);
+let result = await response.text();
+console.log(result);
+
+}) ();
+
