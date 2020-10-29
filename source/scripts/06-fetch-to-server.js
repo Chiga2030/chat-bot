f8d0cc7c79eea2f8324df74f08f812a880f51728
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-10-29 08:10:09 +0300

    class for message rebased toseparate file

---
+++ source/scripts/06-fetch-to-server.js
@@ -0,0 +1,17 @@
+const url = 'http://localhost:3000/request-to-bot';
+
+(async () => {
+
+let response = await fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  body: JSON.stringify(quickMath)
+});
+
+let result = await response.json();
+console.log(result);
+
+}) ();
+
