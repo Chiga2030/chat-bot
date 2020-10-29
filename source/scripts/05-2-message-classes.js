22b48a7f06b06d17773071e5fb8a8aa3b21cf2ed
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-10-29 09:18:24 +0300

    added new classes for user messages

--- source/scripts/05-2-message-classes.js
+++ source/scripts/05-2-message-classes.js
@@ -1,5 +1,20 @@
-class userMessage {
+class StartStopMessage {
 	constructor(comand) {
-		this.comand = this.comand;
+		this.comand = comand;
+	}
+}
+
+class NameMessage extends StartStopMessage {
+	constructor(comand, name) {
+		super(comand);
+		this.name = name;
+	}
+}
+
+class NumMessage extends StartStopMessage {
+	constructor(comand, num1, num2) {
+		super(comand);
+		this.num1 = num1;
+		this.num2 = num2;
 	}
 }
