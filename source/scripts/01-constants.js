d7654b3911405f3a5a2dce16ca95e10ec667bbb0
Антон Ткачук <chiga2030@gmail.com>
2020-10-30 21:59:42 +0300

    rebase variable with dots

--- source/scripts/01-constants.js
+++ source/scripts/01-constants.js
@@ -4,3 +4,4 @@ const submitBtn = searchElement('.chat-window__text-buble-button');
 const chatHistory = searchElement('.chat-window__history');
 const order = makeDecrementor();	// обратный счетчик для упорядочивания сообщений снизу вверх
 let lastBuble;
+const dots = '<span class="dotted-anim">.</span><span class="dotted-anim">.</span><span class="dotted-anim">.</span><span class="dotted-anim">.</span>';	// дефолтное содержимое бабла юзера
