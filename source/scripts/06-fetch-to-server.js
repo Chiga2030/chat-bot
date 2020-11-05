ec3b90fd8c7c1cbcda37298d3530d546fb1e591c
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-11-05 10:20:36 +0300

    remove debug string

--- source/scripts/06-fetch-to-server.js
+++ source/scripts/06-fetch-to-server.js
@@ -10,6 +10,5 @@ const sendMessage =  async (request='string') => {
 	});
 
 	let result = await response.json();
-	console.log('РЕЗУЛЬТАТ FETCH ЗАПРОСА ', result);
-	addMessage(newMessage(result.response.answer, false,'bot'), chatHistory)
+	addMessage(newMessage(result.answer, false,'bot'), chatHistory)
 }
