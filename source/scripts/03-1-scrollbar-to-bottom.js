f25eadeffab916de5c2ed9616cecb595d2516e10
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-11-09 08:39:47 +0300

    drop empty string

--- source/scripts/03-1-scrollbar-to-bottom.js
+++ source/scripts/03-1-scrollbar-to-bottom.js
@@ -1,14 +1,5 @@
-// function makeDecrementor() {
-//   let currentCount = 0;
-
-//   return function() {
-//     return currentCount++;
-//   };
-// }
-
 function scrollToBottom(scrollElem) {
 	const curentHeight = scrollElem.scrollHeight;
 
 	scrollElem.scrollTop = curentHeight;
 }
-
