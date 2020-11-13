a1dca91344d1686e0758bba7abb61108212e8ebf
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-11-13 07:55:17 +0300

    remove defaul value of city and comment string

--- bot-modules/bot-fetch-weather.js
+++ bot-modules/bot-fetch-weather.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 const Message = require('./bot-message-constructor.js');
 
-function getWeather(city = 'Moskow') {
-	// let city = '%D0%BC%D0%BE%D1%81%D0%BA%D0%B2%D0%B0';
+function getWeather(city) {
 	const APIKEY = 'c3f20cd7c63db163cc1fab6a995318f7';
 	const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}`;
 
