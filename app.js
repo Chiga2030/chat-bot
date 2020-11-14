9a1ecf6c533fa5e3f1f36050aa560ac9e4c57e69
Антон Ткачук <chiga2030@gmail.com>
2020-11-14 22:54:51 +0300

    preparing to deploy

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const express = require("express");
 const cors = require('cors');
 // создаем объект приложения
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
+console.log(PORT)
 
 const bot = require('./bot-modules/bot-interface.js');
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
+app.use(express.static("client"));
 
-// определяем обработчик для маршрута "/"
-app.post("/request-to-bot", function (request, response) {
+// определяем обработчик для маршрута пост запроса
+app.post("/request-to-bot", (request, response) => {
     if(!request.body) return response.sendStatus(400);
 
     const botAnswer = new Promise( resolve => resolve(bot(request.body.request)) );
@@ -22,6 +24,5 @@ app.post("/request-to-bot", function (request, response) {
 
 // начинаем прослушивать подключения
 app.listen(PORT, () => {
-	console.log('server has been started');
+	console.log(`server has been started on port: ${PORT}`);
 });
-// app.listen(3000);
