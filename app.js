74fb16f2cd7bb050f4743eafcecaf4815a6ac34a
Антон Ткачук <chiga2030@gmail.com>
2020-11-21 14:51:33 +0300

    remive exchange console log

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 // создаем объект приложения
 const app = express();
 const PORT = process.env.PORT || 3000;
-console.log(PORT)
 
 const bot = require('./bot-modules/bot-interface.js');
 
